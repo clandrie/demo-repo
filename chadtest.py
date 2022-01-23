b7d33cf50f482526cd30fbbf17d2d4ce4667013c
Chad Landrie <clandrie@oakton.edu>
2022-01-23 17:03:55 -0500

    i tried

--- chadtest.py
+++ chadtest.py
@@ -3,8 +3,12 @@ import requests
 
 print("Hello World 10")
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 
 print("Chad Changes Main on accident")
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
+print("Chad changes main")
